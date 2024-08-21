879e1ca48bdcaa0110477abcca64edc4854154be
Felipe Lagos <felipe.devs@gmail.com>
2024-08-21 06:34:15 -0400

    all last waypoints for all vehicles marked in map

--- frontend/src/vehicle/components/ListOfVehicles.tsx
+++ frontend/src/vehicle/components/ListOfVehicles.tsx
@@ -9,15 +9,35 @@ export default function ListOfVehicles() {
 
   const url = `${import.meta.env.VITE_API_ROUTE + "/vehicle"}`;
   const apiKey = `${import.meta.env.VITE_GOOGLE_MAPS_API_KEY}`;
-  
-  const [position, setPosition] = useState({ lat: -33.45694, lng: -70.64827 });
+
+  const [position, setPosition] = useState([{ lat: 0, lng: 0 }]);
   const [filter, setFilter] = useState("");
+
+  type Position = {
+    lat: number;
+    lng: number;
+  };
 
   useEffect(() => {
     fetch(url)
       .then((res) => res.json())
       .then((data) => getAllVehicles(data))
       .catch((err) => console.log(err));
+  }, []);
+
+  useEffect(() => {
+    const all_positions: Position[] = new Array<Position>();
+    vehicles.forEach((vehicle) => {
+      if (vehicle.waypoints.length > 0) {
+        vehicle.waypoints.forEach((waypoint) => {
+          all_positions.push({
+            lat: parseInt(waypoint.latitude),
+            lng: parseInt(waypoint.longitude),
+          });
+        });
+      }
+    });
+    setPosition(all_positions);
   }, []);
 
   return (
@@ -40,36 +60,42 @@ export default function ListOfVehicles() {
             </tr>
           </thead>
           <tbody>
-            {vehicles.filter((item) => {
+            {vehicles
+              .filter((item) => {
                 return item.plate.toLowerCase().includes(filter.toLowerCase());
-            }).map((vehicle) => (
-              <tr key={`vehicle${vehicle.id}`}>
-                <td>{vehicle.plate}</td>
-                <td>
-                  <table>
-                    {vehicle.waypoints?.map((waypoint) => (
-                      <tr
-                        key={`vehicle${vehicle.id}-wp${waypoint.id}`}
-                      >
-                        <td>{waypoint.latitude}</td>
-                        <td>{waypoint.longitude}</td>
-                        <td>{waypoint.sent_at}</td>
-                        <td>
-                          <button
-                            onClick={() => setPosition({
-                              lat: parseInt(waypoint.latitude),
-                              lng: parseInt(waypoint.longitude),
-                            })}
-                          >
-                            Map
-                          </button>
-                        </td>
-                      </tr>
-                    ))}
-                  </table>
-                </td>
-              </tr>
-            ))}
+              })
+              .map((vehicle) => (
+                <tr key={`vehicle${vehicle.id}`}>
+                  <td>{vehicle.plate}</td>
+                  <td>
+                    <table>
+                      <tbody>
+                        {vehicle.waypoints?.map((waypoint) => (
+                          <tr key={`vehicle${vehicle.id}-wp${waypoint.id}`}>
+                            <td>{waypoint.latitude}</td>
+                            <td>{waypoint.longitude}</td>
+                            <td>{waypoint.sent_at}</td>
+                            <td>
+                              <button
+                                onClick={() =>
+                                  setPosition([
+                                    {
+                                      lat: parseInt(waypoint.latitude),
+                                      lng: parseInt(waypoint.longitude),
+                                    },
+                                  ])
+                                }
+                              >
+                                Map
+                              </button>
+                            </td>
+                          </tr>
+                        ))}
+                      </tbody>
+                    </table>
+                  </td>
+                </tr>
+              ))}
           </tbody>
         </table>
       </div>
@@ -80,11 +106,13 @@ export default function ListOfVehicles() {
         <div style={{ height: "50vh", width: "100%" }}>
           <APIProvider apiKey={apiKey}>
             <Map
-              center={position}
-              zoom={5}
+              defaultCenter={position[0] || { lat: 0, lng: 0 }}
+              defaultZoom={1}
               mapId={"vehicleMapper"}
             >
-              <AdvancedMarker position={position} />
+              {position.map((position) => (
+                <AdvancedMarker position={position} />
+              ))}
             </Map>
           </APIProvider>
         </div>
